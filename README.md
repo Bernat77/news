41c3c28fcb4608e75d4abfcb823da960fbfee2b4
Bernat77 <32370258+Bernat77@users.noreply.github.com>
2018-05-24 15:00:30 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,19 +1,4 @@
-# Página de noticias: Documentación.
-
-
-## Link de Rawgit:
-https://rawgit.com/Bernat77/news/0.2/index.html
-
-
-**v0.2**
-La versión 0.2 añade la funcionalidad de cargar los ficheros Json y toda la codificación.
-
-* Añadidos menús en la nav bar (Dropdown de categorías, dropdown de Login).
-* Añadido botón funcional para activar y desactivar el autoscroll.
-* Añadida codificación de la carga de Jsons al hacer scroll hacia abajo del todo.
-* Añadido botón funcional para cargar más noticias, que en cuanto no hay más lo indica en su propio texto.
-* Completada la maquetación general y aplicado el diseño.
-* Completados los json con imágenes.
+## Página de noticias: Documentación.
 
 **v0.1:**
 La versión 0.1 es el diseño de la página, con las noticias precargadas, los anuncios colocados y poco más. Todavía necesita toda la codificación.
--- js/js.js
+++ js/js.js
@@ -1,63 +1,58 @@
 ///////variables
-
-var json=1;
-var autoscroll=true;
+var json = 1;
+var autoscroll = true;
 
 ///////eventos
-$(window).scroll(function (){
- if ($(window).scrollTop() + $(window).height() + 100 >= $(document).height() && autoscroll==true){
-   cargarJson();    
-  }
+$(window).scroll(function() {
+    if ($(window).scrollTop() + $(window).height() + 90 >= $(document).height() && autoscroll == true) {
+        cargarJson();
+    }
 });
 
-$(document).ready( function() {
-	
-$("#auto").click(function(){
-  if(autoscroll){
-    autoscroll=false;
-    $('#auto').text("Activar Auto-Scroll");
-  }else{
-    autoscroll=true;
-    $('#auto').text("Desactivar Auto-Scroll");
-  }
+
+$("#auto").click(function() {
+    if (autoscroll) {
+        autoscroll = false;
+        $('#auto').text("Activar Auto-Scroll");
+    } else {
+        autoscroll = true;
+        $('#auto').text("Desactivar Auto-Scroll");
+    }
 });
 
-$("#cargarmas").click(function(){
-  cargarJson();
-});
-	
-
+$("#cargarmas").click(function() {
+    cargarJson();
 });
 
 ///////funciones
 
-function cargarJson(){
-  
-     if(json<5){
-       $("#cargando").show();
-       $.getJSON("https://rawgit.com/Bernat77/news/master/data/json"+json+".json", function (jsonObject) {
-         añadirJson(jsonObject);
-         $("#cargando").hide();
-         });
-      json++;
-	  row++;
-      }else{
+function cargarJson() {
+
+    if (json < 5) {
+        $("#cargando").show();
+        $.getJSON("https://rawgit.com/Bernat77/news/master/data/json" + json + ".json", function(jsonObject) {
+            añadirJson(jsonObject);
+            $("#cargando").hide();
+        });
+        json++;
+        row++;
+    } else {
         $("#cargarmas").text("No hay más noticias.");
         $("#cargarmas").addClass("disabled");
-      }
-  }
+    }
+}
 
 
-function añadirJson(json){
-  $("#filas").append('<div class="row">'+'</div>');
-    $.each(json, function( i, item ) {
-      $("#filas .row:last").append('<div class="col-sm-12 col-md-6">' 
-	  + '<div class="cuadro">' + '<div class="titulo">' + '<h3 class="text-center">'
-	  +item.titulo+'</h3>' + '</div>' + '<p class="text-right fecha"><i>'+item.fecha
-	  +'</i></p>' + '<img src="'+item.imgs+'" class="img-responsive center-block" alt="'
-	  +item.alt+'">' + '<p class="cuerpo">'+item.cuerpo+'</p>' + '</div>' + '</div>');
+function añadirJson(json) {
+    $("#filas").append('<div class="row">' + '</div>');
+    $.each(json, function(i, item) {
+        $("#filas .row:last").append('<div class="col-sm-12 col-md-6">' +
+            '<div class="cuadro">' + '<div class="titulo">' + '<h3 class="text-center">' +
+            item.titulo + '</h3>' + '</div>' + '<p class="text-right fecha"><i>' + item.fecha +
+            '</i></p>' + '<img src="' + item.imgs + '" class="img-responsive center-block" alt="' +
+            item.alt + '">' + '<p class="cuerpo">' + item.cuerpo + '</p>' + '</div>' + '</div>');
 
-    
-})
-  
+
+    })
+
 }
