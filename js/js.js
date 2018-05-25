b70be4866e4a278d289b959c43a1896eb176a478
Bernat77 <32370258+Bernat77@users.noreply.github.com>
2018-05-25 03:33:59 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@ var autoscroll = true;
 ///////eventos
 $(window).scroll(function() {
     var boton = false;
-    if ($(window).scrollTop() + $(window).height() + 60 >= $(document).height() && autoscroll == true) {
+    if ($(window).scrollTop() + $(window).height() + 80 >= $(document).height() && autoscroll == true) {
         cargarJson(boton);
     }
 });
